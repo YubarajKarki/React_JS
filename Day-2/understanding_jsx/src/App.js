155fd0a3f707ad75cc0c56fa7cdcbf43a90500ad
YubarajKarki <yubrazkarkee7917@gmail.com>
2023-06-10 17:00:16 +0545

    Bootstrap Added and Understanding Props

--- Day-2/understanding_jsx/src/App.js
+++ Day-2/understanding_jsx/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
-let name = "Yubaraj Karki";
+import Navbar from './components/Navbar';
+// let name = "Yubaraj Karki";
 
 function App() {
   return (
     <>
-      <nav>
+
+      {/* <nav>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
@@ -20,10 +20,14 @@ function App() {
         <p>
           JSX stands for JavaScript XML.JSX allows us to write HTML in React.JSX makes it easier to write and add HTML in React.SX allows us to write HTML elements in JavaScript and place them in the DOM without any createElement()  and/or appendChild() methods.JSX converts HTML tags into react elements. You are not required to use JSX, but JSX makes it easier to write React applications. With JSX you can write expressions inside curly braces { }. The expression can be a React variable, or property, or any other valid JavaScript expression. JSX will execute the expression and return the result. The HTML code must be wrapped in ONE top level element.So if you like to write two paragraphs, you must put them inside a parent element, like a div element.JSX will throw an error if the HTML is not correct, or if the HTML misses a parent element.Elements Must be Closed.Attribute class = classNameThe class attribute is a much used attribute in HTML, but since JSX is rendered as JavaScript, and the class keyword is a reserved word in JavaScript, you are not allowed to use it in JSX.Use attribute className instead.Conditions - if statements
           React supports if statements, but not inside JSX.To be able to use conditional statements in JSX, you should put the if statements outside of the JSX, or you could use a ternary expression instead:Option 1:
-          Write if statements outside of the JSX cod
+          Write if statements outside of the JSX code
 
         </p>
-      </div>
+      </div> */} 
+
+{/* <Navbar title= "TextUtils" aboutUtils="About Utils"/> */}
+{/* <Navbar/> */}
+<Navbar title= "TextUtils" />
 
     </>
   );
