32555686e0e6f1e80fa14fec5480eb47fe8018c2
YubarajKarki <yubrazkarkee7917@gmail.com>
2023-06-12 19:16:05 +0545

    Clear Button Added

--- Day-2/understanding_jsx/src/components/TextForm.js
+++ Day-2/understanding_jsx/src/components/TextForm.js
@@ -1,28 +1,38 @@
-import React, {useState} from "react";
+import React, {  useState } from "react";
 
 
 
 export default function TextForm(props) {
+      
+
       const handleUpClick = () => {
             // console.log("Uppercase was clicked: " + text);
             let newText = text.toUpperCase();
             setText(newText);
       }
       const handleLowClick = () => {
-            // console.log("Uppercase was clicked: " + text);
             let newText = text.toLowerCase();
             setText(newText);
       }
 
+      const handleClearClick = () => {
+            let newText= '';
+            setText(newText);
+      }
+          
+      
       const handleOnChange = (event) => {
             // console.log("On Change");
             setText(event.target.value);
       } 
 
       const [text, setText] = useState('');
+
       // text = "New Text"; //Wrong way to change the state
       // setText = ("New Text"); //Correct way to change the state
       
+      
+
       return (
       <>
 <div className="container">
@@ -30,8 +40,10 @@ export default function TextForm(props) {
   <div className="mb-3">
     <textarea className="form-control" value ={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
   </div>
-  <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-  <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+  
 </div>
 
 <div className="container my-3">
