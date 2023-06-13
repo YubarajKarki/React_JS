3bcd985c057a4138eaa277a9563df5d4e56ea452
YubarajKarki <yubrazkarkee7917@gmail.com>
2023-06-13 22:46:26 +0545

    About Component Added

--- Day-2/understanding_jsx/src/App.js
+++ Day-2/understanding_jsx/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 // let name = "Yubaraj Karki";
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -31,9 +32,11 @@ function App() {
 <Navbar title= "TextUtils" />
 
 <div className="container my-3">
-  <TextForm heading= "Enter Text Here" />
-</div>
+  {/* <TextForm heading= "Enter Text Here" />
+ */}
+<About/>
 
+</div>
     </>
   );
 }
