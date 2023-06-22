3673082863779e94a23852452bacbc38690d81f0
Yubaraj Karki <yubrazkarkee7917@gmail.com>
2023-06-22 22:23:58 +0545

    NewsItem Aligned and Structured

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,12 +4,24 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is  News Component
+      <div className= "container my-3">
+        {/* This is  News Component
         <NewsItem />
         <NewsItem />
         <NewsItem />
-        <NewsItem />
+        <NewsItem /> */}
+
+        <h2>NewsAPP - Top Headlines</h2>
+        <div className="row">
+          
+          <div className="col-md-4" description="myDesc"></div>
+          
+          <div className="col-md-4" description="myDesc"></div>
+          
+          <div className="col-md-4" description="myDesc"></div>
+          
+        </div>
+
       </div>
     )
   }
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class NewsItem extends Component {
   render() {
     return (
-      <div>This is News Item Component1111</div>
+      <div className= "card" style={{ width: "18rem" }}>
+        <div className= "card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetails" className='btn btn-primary'>Go Somewhere</a>
+        </div>
+      </div>
     )
   }
 }
