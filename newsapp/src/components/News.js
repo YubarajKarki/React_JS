ba3cbcfed87e07f3c66c8003c5f9a54a9ac8a0e5
Yubaraj Karki <yubrazkarkee7917@gmail.com>
2023-06-23 23:20:23 +0545

    Fetching API Key from NewsAPI

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -5,23 +5,14 @@ export class News extends Component {
   render() {
     return (
       <div className= "container my-3">
-        {/* This is  News Component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem /> */}
+        
 
         <h2>NewsAPP - Top Headlines</h2>
         <div className="row">
-          
           <div className="col-md-4" description="myDesc"></div>
-          
           <div className="col-md-4" description="myDesc"></div>
-          
           <div className="col-md-4" description="myDesc"></div>
-          
         </div>
-
       </div>
     )
   }
